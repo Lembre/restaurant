1a3561a90956420f3ceca03b1ab532861cc3c05d
Lembre <huizy_1587834945@sina.com>
2018-09-25 18:56:30 +0800

    SSM+Shiro整合

---
+++ src/main/java/shiro/CustomRealm.java
@@ -0,0 +1,21 @@
+package shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by Lembre on 2018.9.25
+ */
+public class CustomRealm extends AuthorizingRealm {
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
